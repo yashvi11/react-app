a9de560e0627fd3c55b3e8b2dba3f390c88f2b43
Yashvi11 <yashviraheja11@gmail.com>
2019-12-09 13:59:33 +0530

    Smart & Dumb: Added ‘PhoneDirectory’ as smart component, renamed ‘App…

--- src/PhoneDirectory.js
+++ src/PhoneDirectory.js
@@ -1,46 +1,45 @@
 
-import  React, {Component } from 'react';
-import Header from './header.js';
-//import './ShowSubscribers.css';
-import './PhoneDirectory.css';
-class App extends Component{
-  constructor() {
-    super();
-    this.state = {
-      subscribersListToShow: []
+import React, { Component } from 'react';
+import AddSubscriber from './AddSubscriber';
+import ShowSubscribers from './ShowSubscribers';
+
+class PhoneDirectory extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            subscribersList: [
+                {
+                    id: 1,
+                    name: "Shilpa Bhat",
+                    phone: "9999999999"
+                },
+                {
+                    id: 2,
+                    name: "Srishti Gupta",
+                    phone: "8888888888"
+                }
+            ]
+        }
     }
-  }
 
-  render() {
+    addSubscriberHandler = (newSubscriber) => {
+        let subscribersList = this.state.subscribersList;
+        if (subscribersList.length > 0) {
+            newSubscriber.id = subscribersList[subscribersList.length - 1].id + 1;
+        } else {
+            newSubscriber.id = 1;
+        }
+        subscribersList.push(newSubscriber);
+        this.setState({ subscribersList: subscribersList });
+    }
 
-return(
-      <div>
-        <Header heading="Phone Directory" />
-        <div className="component-body-container">
-          <button className="custom-btn add-btn">Add</button>
-
-          <div className="grid-container heading-container">
-            <span className="grid-item name-heading">Name</span>
-            <span className="grid-item phone-heading">Phone</span>
-          </div>
-          {
-            
-              this.state.subscribersListToShow.map(sub => {
-              return <div key={sub.id} className="grid-container">
-                <span className="grid-item">{sub.name}</span>
-                <span className="grid-item">{sub.phone}</span>
-                <span className="grid-item action-btn-container">
-                  <button className="custom-btn delete-btn">Delete</button>
-                </span>
-                 </div>
-            })
-          }
-          
-        </div>
-      </div>
-    );
+    render() {
+        return (
+            // <AddSubscriber addSubscriberHandler={this.addSubscriberHandler} />
+            <ShowSubscribers subscribersList={this.state.subscribersList} />
+        )
+    }
 }
-}
-  
 
-export default App;
+export default PhoneDirectory; 
--- src/ShowSubscribers.js
+++ src/ShowSubscribers.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
-import Header from './Header.js';
-import './ShowSubscribers.css';
+import Header from './header.js';
+import './PhoneDirectory.css';
 class ShowSubscribers extends Component {
+    render(){
     return (
         <div>
           <Header heading="Phone Directory" />
+          <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+
           </div>
 
           {
-            this.props.subscribersList.map(sub => {
+             this.props.subscribersList.map(sub => {
                 return <div key={sub.id} className="grid-container">
                   <span className="grid-item">{sub.name}</span>
                   <span className="grid-item">{sub.phone}</span>
-                }
+                  <span className="grid-item action-btn-container">
+                    <button className="custom-btn delete-btn">Delete</button>
+                  </span>
+                </div>
+              })
             }
+          </div>
+        </div>
+      );
+    }
+  }
+    
             export default ShowSubscribers;
