84fa5a0c8cbf94fd96e7b3787d44c101ee8fa1fd
Yashvi11 <yashviraheja11@gmail.com>
2019-12-10 15:00:42 +0530

    Updates

---
+++ precache-manifest.677059b37fa0accda4163692d1503938.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9922efd3370631e7d111ae2368178f2f",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "94ffdc27e941e0c41f5e",
+    "url": "/react-app/static/css/main.b025edb1.chunk.css"
+  },
+  {
+    "revision": "ad88fc23cbbb8365a20c",
+    "url": "/react-app/static/js/2.0e4944c0.chunk.js"
+  },
+  {
+    "revision": "94ffdc27e941e0c41f5e",
+    "url": "/react-app/static/js/main.786bb00c.chunk.js"
+  },
+  {
+    "revision": "66119acb54464a30acdb",
+    "url": "/react-app/static/js/runtime-main.5b661208.js"
+  }
+]);
